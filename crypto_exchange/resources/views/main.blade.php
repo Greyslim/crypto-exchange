cf2d5d5b351f5b09cd0a8451be5c1c8834481d67
Greyslim <kalinin.kostik@gmail.com>
2023-01-03 20:56:08 +0000

    Fix html lang

--- crypto_exchange/resources/views/main.blade.php
+++ crypto_exchange/resources/views/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,5 +9,11 @@
 </head>
 <body>
 <h1>Main</h1>
+<form class="col-3 offset-4 border-4 border rounded" method="GET" action="{{route("user.logout")}}">
+    @csrf
+    <div class="form-group">
+        <button class="btn btn-lg btn-primary" type="submit" name="sendMe" value="1">Logout</button>
+    </div>
+</form>
 </body>
 </html>
